33f5b53f8e15b40b4c24773b0ac0db3f7cd958c2
[olivier-keutgens] <[keutgensolivier@gmail.com]>
2020-12-03 23:40:56 +0100

    css fixes

--- src/components/index/contact.js
+++ src/components/index/contact.js
@@ -11,8 +11,8 @@ const Contact = (props) => {
                     <div className={contactStyles.info}>
                         <p className={`text-normal ${contactStyles.infoDesc}`}>{props.invitation}</p>
                         <p className="text-normal">Voie Pirson 9A, 4877 Olne</p>
-                        <p className="text-normal">0431 83 10 87</p>
-                        <p className="text-normal">atelier.keutgens@gmail.com</p>
+                        <p className="text-normal--black">0431 83 10 87</p>
+                        <p className="text-normal--black">atelier.keutgens@gmail.com</p>
                     </div>
                 </div>
                 <div className={contactStyles.contact}>
@@ -28,8 +28,8 @@ const Contact = (props) => {
                         <h3 className="h2">Michel Keutgens</h3>
                         <p className={`text-important ${contactStyles.infoTitle}`}>Architecte</p>
                         <p className={`text-normal ${contactStyles.infoDesc}`}>{props.descriptionMichel}</p>
-                        <p className="text-normal">michel.keutgens@gmail.com</p>
-                        <p className="text-normal">0471 83 10 87</p>
+                        <p className="text-normal--black">michel.keutgens@gmail.com</p>
+                        <p className="text-normal--black">0471 83 10 87</p>
                     </div>
                 </div>
                 <div className={contactStyles.contact}>
@@ -45,8 +45,8 @@ const Contact = (props) => {
                         <h3 className="h2">Sabina Ianieri</h3>
                         <p className={`text-important ${contactStyles.infoTitle}`}>Architecte d'intérieur</p>
                         <p className={`text-normal ${contactStyles.infoDesc}`}>{props.descriptionSabina}</p>
-                        <p className="text-normal">sabina.ianieri@gmail.com</p>
-                        <p className="text-normal">0471 83 10 87</p>
+                        <p className="text-normal--black">sabina.ianieri@gmail.com</p>
+                        <p className="text-normal--black">0471 83 10 87</p>
                     </div>
                 </div>
             </div>
